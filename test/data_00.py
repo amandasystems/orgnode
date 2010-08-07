bcfed19e116be3c5ea4f5d37077f93804112ed16
Takafumi Arakaki <devnull@localhost>
2010-08-07 19:28:13 +0200

    more tests for tags_inher

--- test/data_00.py
+++ test/data_00.py
@@ -1,23 +1,24 @@
-keys1 = ['level', 'heading', 'todo', 'tags', 'tags_inher']
-keys2 = ['level', 'heading']
+def nodedict(i, level, todo='', tags=set([]), tags_inher=set([])):
+    return dict(
+        heading="Heading %d"%i, level=level, tags=tags, tags_inher=tags_inher)
 
 def tags(nums):
     return set(['TAG%d'%i for i in nums])
 
 data = [
-    dict(zip(keys1, vals)) for vals in
-    [[1, 'Heading (Level 1) 1', 'TODO1', tags([1]), tags(range(1,2))],
-     [2, 'Heading (Level 2) 1', 'TODO2', tags([2]), tags(range(1,3))],
-     [3, 'Heading (Level 3) 1', 'TODO3', tags([3]), tags(range(1,4))],
-     [4, 'Heading (Level 4) 1', 'TODO4', tags([4]), tags(range(1,5))],
-     ]
-    ] + [
-    dict(zip(keys2, vals)) for vals in
-    [[2, 'Heading (Level 2) 2'],
-     [2, 'Heading (Level 2) 3'],
-     [1, 'Heading (Level 1) 2'],
-     [2, 'Heading (Level 2) 4'],
-     [2, 'Heading (Level 2) 5'],
-     [1, 'Heading (Level 1) 3'],
-     ]
+    nodedict(i, *vals) for (i, vals) in enumerate(
+        [[1, 'TODO1', tags([1]), tags(range(1,2))],
+         [2, 'TODO2', tags([2]), tags(range(1,3))],
+         [3, 'TODO3', tags([3]), tags(range(1,4))],
+         [4, 'TODO4', tags([4]), tags(range(1,5))],
+         [2, '', tags([]), tags([1])],
+         [2, '', tags([]), tags([1])],
+         [1, '', tags([2]), tags([2])],
+         [2, '', tags([2]), tags([2])],
+         [3, '', tags([]), tags([2])],
+         [5, '', tags([3,4]), tags([2,3,4])],
+         [4, '', tags([1]), tags([1,2])],
+         [2, '', tags([]), tags([2])],
+         [1],
+         ])
     ]
