208524a85f2b23325f1a98b80dda319893d084b3
Takafumi Arakaki <devnull@localhost>
2010-08-07 19:40:33 +0200

    move test_get_daterangelist to test/test.py

--- test/test.py
+++ test/test.py
@@ -2,7 +2,7 @@ import os
 from glob import glob
 from nose.tools import eq_
 
-from Orgnode import Orgnode, makelist
+from Orgnode import Orgnode, makelist, get_daterangelist, get_datetime
 
 TESTDIR = os.path.dirname(__file__)
 
@@ -38,3 +38,36 @@ def test_data():
     for oname in glob(os.path.join(TESTDIR, 'data_*.org')):
         dataname = os.path.basename(oname)[:-4]
         yield (check_data, dataname)
+
+
+def test_get_daterangelist():
+    datefmt0 = "<%(Y)04d-%(M)02d-%(D)02d %(d)s>"
+    datefmt1 = "<%(Y)04d-%(M)02d-%(D)02d %(d)s %(h)02d:%(m)02d>"
+
+    dates = [
+        dict(Y=2010, M=6, D=21, d='Mon', h=12, m=0),
+        dict(Y=2010, M=6, D=21, d='Mon', h=13, m=0),
+        dict(Y=2010, M=6, D=21, d='Mon', h=None, m=None),
+        ]
+
+    datestr = " ".join([
+        datefmt1 % dates[0],
+        datefmt1 % dates[1],
+        datefmt0 % dates[2],
+        ("%s--%s" % (datefmt1 % dates[0], datefmt1 % dates[1]) )
+        ])
+
+    desired_datelist = [ get_datetime(d['Y'], d['M'], d['D'], d['h'], d['m'])
+                         for d in dates ]
+    d0 = dates[0]
+    d1 = dates[1]
+    desired_rangelist = [
+        (get_datetime(d0['Y'], d0['M'], d0['D'], d0['h'], d0['m']),
+         get_datetime(d1['Y'], d1['M'], d1['D'], d1['h'], d1['m']))]
+
+    for context in ["%s", " %s", "%s ", "aaa%sbbb"]:
+        (datelist, rangelist) =  get_daterangelist(context % datestr)
+        for (des, act) in zip(desired_datelist, datelist):
+            assert des == act
+        for (des, act) in zip(desired_rangelist, rangelist):
+            assert des == act
