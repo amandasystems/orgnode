f5d3233aedcd6a2f29192e71f59a80a18e174bb3
Albin Stjerna <albin@eval.nu>
2012-12-05 16:16:35 +0100

    Add tests for links (currently failing).

---
+++ test/data_02.py
@@ -0,0 +1,13 @@
+node1 = dict(
+    Heading = "A node with no TODO and [[http://google.com][a link]]",
+    Body = "")
+
+node2 = dict(
+    Heading = "A node with a raw link: http://google.com",
+    Body = "")
+
+node3 = dict(
+    Heading = "A node with [some brackets]",
+    Body = "")
+
+data = [node1, node2, node3]
